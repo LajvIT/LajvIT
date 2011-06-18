d820fb63fdae294dad198c00dd747afd4e8e6c09
pviking <pviking@445650e8-d2b1-4875-bca8-69660223506a>
2011-06-18 08:38:13 +0000

    Added list support for plots
    git-svn-id: svn+ssh://svn.djupfeldt.se/home/svn/khanmalan/trunk@33 445650e8-d2b1-4875-bca8-69660223506a

---
+++ site/views/plot/tmpl/listplots.php
@@ -0,0 +1,46 @@
+<?php
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<h1>Intriger</h1>
+
+<form action="index.php" method="post" enctype="multipart/form-data" name="plotDefaultForm">
+	<table>
+		<tbody>
+
+<?php
+			foreach ($this->plots as $plot) {
+				if ($this->mergedrole->character_setstatus || $plot->creatorpersonid == $this->person->id ) {
+				?>
+			<tr>
+				<td>
+					<a href="index.php?option=com_lajvit&view=plot&layout=editplot&eid=<?php echo $this->eventId; ?>&pid=<?php echo $plot->id; ?>">
+						<?php echo $plot->heading;?>
+					</a>
+				</td><td><?php echo $plot->description; ?></td>
+			</tr>
+<?php 	}
+			} ?>
+			<tr><td colspan="2"></td></tr>
+			<tr><td colspan="2">
+				<a href="index.php?option=com_lajvit&view=plot&layout=editplot&eid=<?php echo $this->eventId; ?>">
+						Lägg till ny intrig
+					</a>
+			</td></tr>
+
+		</tbody>
+	</table>
+<?php
+	if ($this->mergedrole->character_setstatus || $this->mergedrole->registration_setstatus || $this->mergedrole->registration_setrole) {
+?>
+	<input type="hidden" name="option" value="com_lajvit" />
+	<input type="hidden" name="task" value="savePlotObject" />
+	<input type="hidden" name="controller" value="plot" />
+	<input type="hidden" name="eid" value="<? echo $this->eventId; ?>" />
+<?php
+	}
+?>
+</form>
+
