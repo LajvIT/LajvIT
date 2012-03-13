539b33d4a9897aa6208de2ac6baaf5afe868d58a
Pontus Viking <pontus.viking@meradan.se>
2012-03-13 11:21:01 +0100

    Checkstyle

--- site/controllers/registrations.php
+++ site/controllers/registrations.php
@@ -1,87 +1,87 @@
 <?php
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
+/**
+ * Controller for registrations.
+ */
 class LajvITControllerRegistrations extends LajvITController {
-	function save() {
-		$errlink = 'index.php?option=com_lajvit&view=registrations';
-		$errlink.= '&Itemid='.JRequest::getInt('Itemid', 0);
+  function save() {
+    $errlink = 'index.php?option=com_lajvit&view=registrations';
+    $errlink .= '&Itemid='.JRequest::getInt('Itemid', 0);
 
-		$model = &$this->getModel();
-		$db = &JFactory::getDBO();
+    $model = &$this->getModel();
+    $db = &JFactory::getDBO();
 
-		$eventid = JRequest::getInt('eid', -1);
-		$eventrole = $model->getRoleForEvent($eventid);
+    $eventid = JRequest::getInt('eid', -1);
+    $eventrole = $model->getRoleForEvent($eventid);
 
-		$characterlist = split(',', JRequest::getString('cid', ''));
-		
-		echo JRequest::getString('cid', '') . "<br>";
+    $characterlist = preg_split('/,/', JRequest::getString('cid', ''));
 
-		foreach ($characterlist as $charidstr) {
-			$charid = (int) $charidstr;
-			if (is_null($charid) || $charid <= 0) {
-				continue;
-			}
+    echo JRequest::getString('cid', '') . "<br>";
 
-			$personid = JRequest::getInt('pid_'.$charid, -1);
-			
-			
-			$crole = $model->getRoleForChara($eventid, $charid);
-			$role = $model->mergeRoles($eventrole, $crole);
-			
+    foreach ($characterlist as $charidstr) {
+      $charid = (int) $charidstr;
+      if (is_null($charid) || $charid <= 0) {
+        continue;
+      }
 
-			echo $charid . " {";
-			foreach (get_object_vars($role) as $k => $v) {
-				echo $k . ": " . ($v ? 1 : 0) . " ";
-			}
-			echo "}<br>";
+      $personid = JRequest::getInt('pid_'.$charid, -1);
 
+      $crole = $model->getRoleForChara($eventid, $charid);
+      $role = $model->mergeRoles($eventrole, $crole);
 
-			$statusid = JRequest::getInt('characterstatus_'.$charid, -1);
-			if ($role->character_setstatus && $statusid > 0) {
-				$query = 'UPDATE #__lit_registrationchara SET statusid='.$db->getEscaped($statusid).' WHERE eventid='.$db->getEscaped($eventid).' AND personid='.$db->getEscaped($personid).' AND charaid='.$db->getEscaped($charid).";\n";
+      echo $charid . " {";
+      foreach (get_object_vars($role) as $k => $v) {
+        echo $k . ": " . (($v) ? 1 : 0) . " ";
+      }
+      echo "}<br>";
 
-				$db->setQuery($query);
+      $statusid = JRequest::getInt('characterstatus_'.$charid, -1);
+      if ($role->character_setstatus && $statusid > 0) {
+        $query = 'UPDATE #__lit_registrationchara SET statusid='.$db->getEscaped($statusid).' WHERE eventid='.$db->getEscaped($eventid).' AND personid='.$db->getEscaped($personid).' AND charaid='.$db->getEscaped($charid).";\n";
 
-				if (!$db->query()) {
-					echo '<h1>'.$db->getErrorMsg().'</h1>';
-					//				$this->setRedirect($errlink, $db->getErrorMsg());
-					return;
-				}
-			}
+        $db->setQuery($query);
 
-			$payment = JRequest::getInt('payment_'.$charid, -1);
-			if ($eventrole->registration_setstatus && $personid > 0 && $payment >= 0) {
-				$query = 'UPDATE #__lit_registration SET payment='.$db->getEscaped($payment).', timeofpayment=NOW() WHERE eventid='.$db->getEscaped($eventid).' AND personid='.$db->getEscaped($personid).";\n";
+        if (!$db->query()) {
+          echo '<h1>'.$db->getErrorMsg().'</h1>';
+          //        $this->setRedirect($errlink, $db->getErrorMsg());
+          return;
+        }
+      }
 
-				$db->setQuery($query);
+      $payment = JRequest::getInt('payment_'.$charid, -1);
+      if ($eventrole->registration_setstatus && $personid > 0 && $payment >= 0) {
+        $query = 'UPDATE #__lit_registration SET payment='.$db->getEscaped($payment).', timeofpayment=NOW() WHERE eventid='.$db->getEscaped($eventid).' AND personid='.$db->getEscaped($personid).";\n";
 
-				if (!$db->query()) {
-					echo '<h1>'.$db->getErrorMsg().'</h1>';
-					//				$this->setRedirect($errlink, $db->getErrorMsg());
-					return;
-				}
-			}
+        $db->setQuery($query);
 
-			$confirmationid = JRequest::getInt('confirmationid_'.$charid, -1);
-			if ($eventrole->registration_setstatus && $personid > 0 && $confirmationid > 0) {
-				$query = 'UPDATE #__lit_registration SET confirmationid='.$db->getEscaped($confirmationid).', timeofconfirmation=NOW() WHERE eventid='.$db->getEscaped($eventid).' AND personid='.$db->getEscaped($personid).";\n";
+        if (!$db->query()) {
+          echo '<h1>'.$db->getErrorMsg().'</h1>';
+          //        $this->setRedirect($errlink, $db->getErrorMsg());
+          return;
+        }
+      }
 
-				$db->setQuery($query);
+      $confirmationid = JRequest::getInt('confirmationid_'.$charid, -1);
+      if ($eventrole->registration_setstatus && $personid > 0 && $confirmationid > 0) {
+        $query = 'UPDATE #__lit_registration SET confirmationid='.$db->getEscaped($confirmationid).', timeofconfirmation=NOW() WHERE eventid='.$db->getEscaped($eventid).' AND personid='.$db->getEscaped($personid).";\n";
 
-				if (!$db->query()) {
-					echo '<h1>'.$db->getErrorMsg().'</h1>';
-					//				$this->setRedirect($errlink, $db->getErrorMsg());
-					return;
-				}
-			}
+        $db->setQuery($query);
 
-			echo '<h1>'.$charid.'</h1>';
-			echo $personid.' '.$statusid.' '.$payment.' '.$confirmationid.'<br>';
-		}
+        if (!$db->query()) {
+          echo '<h1>'.$db->getErrorMsg().'</h1>';
+          //        $this->setRedirect($errlink, $db->getErrorMsg());
+          return;
+        }
+      }
 
-		$oklink = 'index.php?option=com_lajvit&view=registrations&eid='.$eventid;
-		$oklink.= '&Itemid='.JRequest::getInt('Itemid', 0);
-		$this->setRedirect($oklink);
-	}
+      echo '<h1>'.$charid.'</h1>';
+      echo $personid.' '.$statusid.' '.$payment.' '.$confirmationid.'<br>';
+    }
+
+    $oklink = 'index.php?option=com_lajvit&view=registrations&eid='.$eventid;
+    $oklink .= '&Itemid='.JRequest::getInt('Itemid', 0);
+    $this->setRedirect($oklink);
+  }
 }
