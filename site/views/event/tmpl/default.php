6c1774f3dc788407ddbe738b34ac16087ddf2ff8
Pontus Viking <pontus.viking@meradan.se>
2012-03-16 09:08:46 +0100

    Checkstyle fixes

--- site/views/event/tmpl/default.php
+++ site/views/event/tmpl/default.php
@@ -1,58 +1,61 @@
 <?php
-
-// No direct access
-
 defined('_JEXEC') or die('Restricted access'); ?>
-
 
 <h1>
   Mina Arrangemang
-  <?php if (TRUE) { ?>
+<?php if (TRUE) { ?>
   &nbsp;<a href="index.php?option=com_lajvit&view=event&Itemid=<?php echo $this->itemid; ?>&layout=add" title="Lägg till arrangemang"><img
     src="components/com_lajvit/new_organizer.gif" alt="Lägg till arrangemang" /> </a>
-  <?php  } ?>
+  <?php
+}
+  ?>
 </h1>
 
-<?php  foreach ($this->events as $event) { ?>
-    <h2><?php echo $event->name; ?>&nbsp;<a href="<?php echo $event->url; ?>" title="Info"><img src="components/com_lajvit/info.png" alt="Info"/></a>
-<?php    if ($event->role->registration_list || $event->role->character_list) { ?>
-      &nbsp;<a href="index.php?option=com_lajvit&view=registrations&eid=<?php echo $event->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Anmälningar"><img src="components/com_lajvit/list.png" alt="Anmälningar"/></a>
-<?php    }
-    if (false) { ?>
-      &nbsp;<a href="event_edit.html" title="Redigera arrangemang"><img src="components/com_lajvit/edit_organizer.gif" alt="Redigera arrangemang"/></a>
-<?php    } ?>
-    </h2>
-    <p>Start: <?php echo $event->startdate; ?> Slut: <?php echo $event->enddate; ?></p>
+<?php
+foreach ($this->events as $event) { ?>
+  <h2><?php echo $event->name; ?>&nbsp;<a href="<?php echo $event->url; ?>" title="Info">
+    <img src="components/com_lajvit/info.png" alt="Info"/></a>
+  <?php
+  if ($event->role->registration_list || $event->role->character_list) { ?>
+    &nbsp;<a href="index.php?option=com_lajvit&view=registrations&eid=<?php echo $event->id; ?>
+      &Itemid=<?php echo $this->itemid; ?>" title="Anmälningar">
+    <img src="components/com_lajvit/list.png" alt="Anmälningar"/></a><?php
+  }
+  if (FALSE) { ?>
+      &nbsp;<a href="event_edit.html" title="Redigera arrangemang">
+      <img src="components/com_lajvit/edit_organizer.gif" alt="Redigera arrangemang"/></a><?php
+  } ?>
+  </h2>
+  <p>Start: <?php echo $event->startdate; ?> Slut: <?php echo $event->enddate; ?></p>
 
-<?php    if (!$event->registered) { ?>
-<p><strong>Ej Registrerad.&nbsp;<a href="index.php?option=com_lajvit&view=event&layout=register&eid=<?php echo $event->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Registrera"><img src="components/com_lajvit/new.gif" alt="Registrera"/></a></strong></p>
-<?php    } else { ?>
-      <p><strong>
-        Betalning: <?php echo $event->confirmationname; ?>
-        (<?php echo $event->payment; ?> kr)
+  <?php
+  if (!$event->registered) { ?>
+    <p><strong>Ej Registrerad.&nbsp;<a href="index.php?option=com_lajvit&view=event&layout=register&eid=<?php echo $event->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Registrera"><img src="components/com_lajvit/new.gif" alt="Registrera"/></a></strong></p><?php
+  } else { ?>
+    <p><strong>Betalning: <?php
+    echo $event->confirmationname;
+    echo '(' . $event->payment . ' kr)'; ?></strong></p><?php
+    foreach ($event->characters as $char) { ?>
+      <p><strong><?php
+      echo $char->knownas . ' - ' . $char->statusname;
+      if (FALSE) { ?>
+        &nbsp;<a href="index.php?option=com_lajvit&view=character&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Info"><img src="components/com_lajvit/info.png" alt="Info"/></a><?php
+      } ?>
+      &nbsp;<a href="index.php?option=com_lajvit&view=character&layout=edit&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Redigera karaktär"><img src="components/com_lajvit/edit.gif" alt="Redigera karaktär"/></a>
+      &nbsp;<a href="index.php?option=com_lajvit&view=character&layout=delete&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Ta bort karaktär"><img src="components/com_lajvit/delete.gif" alt="Ta bort karaktär"/></a>
+      &nbsp;<a href="index.php?option=com_lajvit&view=plot&layout=listdistributedplots&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Intriger för karaktär"><img src="components/com_lajvit/plot.png" alt="Intriger för karaktär"/></a>
       </strong></p>
-
-<?php      foreach ($event->characters as $char) { ?>
-        <p><strong>
-          <?php echo $char->knownas; ?> - <?php echo $char->statusname; ?>
-<?php          if (false) { ?>
-            &nbsp;<a href="index.php?option=com_lajvit&view=character&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Info"><img src="components/com_lajvit/info.png" alt="Info"/></a>
-<?php          } ?>
-          &nbsp;<a href="index.php?option=com_lajvit&view=character&layout=edit&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Redigera karaktär"><img src="components/com_lajvit/edit.gif" alt="Redigera karaktär"/></a>
-          &nbsp;<a href="index.php?option=com_lajvit&view=character&layout=delete&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Ta bort karaktär"><img src="components/com_lajvit/delete.gif" alt="Ta bort karaktär"/></a>
-          &nbsp;<a href="index.php?option=com_lajvit&view=plot&layout=listdistributedplots&eid=<?php echo $event->id; ?>&cid=<?php echo $char->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Intriger för karaktär"><img src="components/com_lajvit/plot.png" alt="Intriger för karaktär"/></a>
-        </strong></p>
-        <p>
-          <?php echo $char->culturename;?> - <?php echo $char->conceptname; ?>
-<?php          if (!is_null($char->concepttext)) { ?>
-            (<?php echo $char->concepttext; ?>)
-<?php          } ?>
-          </p>
-<?php      } ?>
+      <p><?php
+      echo $char->culturename . ' - ' . $char->conceptname;
+      if (!is_null($char->concepttext)) {
+        echo '(' .$char->concepttext . ')';
+      } ?>
+      </p><?php
+    } ?>
 
       <p><strong>
         Lägg till karaktär
         &nbsp;<a href="index.php?option=com_lajvit&view=character&layout=create&eid=<?php echo $event->id; ?>&Itemid=<?php echo $this->itemid; ?>" title="Lägg till karaktär"><img src="components/com_lajvit/new.gif" alt="Lägg till karaktär"/></a>
-      </strong></p>
-<?php    } ?>
-<?php  } ?>
+      </strong></p><?php
+  }
+} ?>
