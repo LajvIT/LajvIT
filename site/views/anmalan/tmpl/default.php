d0b0c5f8dc2005293798088c11cbd92d4a999e95
Pontus Viking <pontus.viking@meradan.se>
2012-03-16 10:44:18 +0100

    Checkstyle fixes

--- site/views/anmalan/tmpl/default.php
+++ site/views/anmalan/tmpl/default.php
@@ -1,6 +1,3 @@
 <?php
- 
-// No direct access
- 
 defined('_JEXEC') or die('Restricted access'); ?>
 <h1><?php echo $this->greeting; ?></h1>
--- site/views/anmalan/view.html.php
+++ site/views/anmalan/view.html.php
@@ -1,32 +1,17 @@
 <?php
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.application.component.view');
+
 /**
- * @package    Joomla.Tutorials
- * @subpackage Components
- * @link http://docs.joomla.org/Developing_a_Model-View-Controller_Component_-_Part_1
- * @license    GNU/GPL
-*/
- 
-// no direct access
- 
-defined( '_JEXEC' ) or die( 'Restricted access' );
- 
-jimport( 'joomla.application.component.view');
- 
-/**
- * HTML View class for the HelloWorld Component
- *
- * @package    HelloWorld
+ * View for anmalan.
  */
- 
 class LajvITViewAnmalan extends JView {
-    function display($tpl = null) {
-    	$model = &$this->getModel();
-		$greeting = $model->getGreeting();
-		
-        $this->assignRef( 'greeting', $greeting );
-        
-		$this->assignRef('itemid', JRequest::getInt('Itemid', 0));
- 
-        parent::display($tpl);
-    }
+  function display($tpl = NULL) {
+    $model = &$this->getModel();
+    $greeting = $model->getGreeting();
+    $this->assignRef( 'greeting', $greeting );
+    $this->assignRef('itemid', JRequest::getInt('Itemid', 0));
+
+    parent::display($tpl);
+  }
 }
