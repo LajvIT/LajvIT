bf5ea638fe6caa3442c24aadfb3c79ebb266a256
Pontus Viking <pontus.viking@meradan.se>
2013-01-24 06:58:30 +0100

    Debug information for ACL on group listings

--- site/views/groups/tmpl/default.php
+++ site/views/groups/tmpl/default.php
@@ -6,13 +6,16 @@ defined('_JEXEC') or die('Restricted access'); ?>
 </h1>
 
 <?php
-foreach ($this->items as $item) { ?>
+$user = JFactory::getUser();
+foreach ($this->items as $item) {
+$assetName = "com_lajvit.group." . $item->id; ?>
   <div class="group">
     <div class="container">
       <div class="infoText">
         <a href="index.php/component/lajvit/?view=group&groupId=<?php echo $item->id?>">
         <?php echo $item->name; ?></a>
       </div>
+      <div class="icon delete_group"><a class="icon" href="index.php?option=com_lajvit&view=group&layout=delete&eid=<?php echo $event->id; ?>" title="Ta bort grupp"></a></div>
     </div>
     <div class="container">
       <div class="text">Gruppledare: <?php echo $item->groupLeaderPersonName;?></div>
@@ -26,6 +29,13 @@ foreach ($this->items as $item) { ?>
     </div>
     <div class="container">
       <div class="text"><?php echo $item->description; ?></div>
+    </div>
+    <div class="container">
+      <div class="infoText">View:</div><div class="text"><?php echo $user->authorise('lajvit.view.visible', $assetName); ?></div>
+      <div class="infoText">View hidden:</div><div class="text"><?php echo $user->authorise('lajvit.view.hidden', $assetName); ?></div>
+      <div class="infoText">Create:</div><div class="text"><?php echo $user->authorise('core.create', $assetName); ?></div>
+      <div class="infoText">Edit:</div><div class="text"><?php echo $user->authorise('core.edit', $assetName); ?></div>
+      <div class="infoText">Edit own:</div><div class="text"><?php echo $user->authorise('core.edit.own', $assetName); ?></div>
     </div>
   </div>
 <?php } ?>
