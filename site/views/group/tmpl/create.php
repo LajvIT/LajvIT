be1de67bda6d4a37147ffeb42a1cb6f124b9799f
Pontus Viking <pontus.viking@meradan.se>
2013-03-16 17:20:28 +0100

    #69 Fixed missing public/private description fields when creating a group

--- site/views/group/tmpl/create.php
+++ site/views/group/tmpl/create.php
@@ -9,31 +9,35 @@ defined('_JEXEC') or die('Restricted access'); ?>
   <table>
     <tbody>
       <tr>
-        <td><strong>Namn:</strong></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_NAME');?>:</strong></td>
         <td><input type="text" name="groupName" value="" size="40"></td>
       </tr>
       <tr>
-        <td><strong>Beskrivning:</strong></td>
-        <td><textArea name="groupDescription" cols="40" rows="5" ></textArea></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_DESCRIPTION');?>:</strong></td>
+        <td><textArea name="groupPublicDescription" cols="40" rows="5" ></textArea></td>
       </tr>
       <tr>
-        <td><strong>Max antal medlemmar:</strong></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_DESCRIPTION_PRIVATE');?>:</strong></td>
+        <td><textArea name="groupPrivateDescription" cols="40" rows="5" ></textArea></td>
+      </tr>
+      <tr>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_MAX_NO_MEMBERS');?>:</strong></td>
         <td><input type="text" name="groupMaxParticipants" value="" size="3"></td>
       </tr>
       <tr>
-        <td><strong>Förväntat antal deltagare:</strong></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_EXPECTED_NO_MEMBERS');?>:</strong></td>
         <td><input type="text" name="groupExpectedParticipants" value="" size="3"></td>
       </tr>
       <tr>
-        <td><strong>Information till arrangör:</strong></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_INFO_FOR_ORGANIZER');?>:</strong></td>
         <td><textArea name="groupAdminInfo" cols="40" rows="5" ></textArea></td>
       </tr>
       <tr>
-        <td><strong>Hemsida:</strong></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_HOMEPAGE');?>:</strong></td>
         <td><input type="text" name="groupUrl" value="" size="40"></td>
       </tr>
       <tr>
-        <td><strong>Fraktion:</strong></td>
+        <td><strong><?php echo JText::_('COM_LAJVIT_GROUP_FACTION');?>:</strong></td>
         <td><select name="groupFaction"><?php
 foreach ($this->factions as $faction) {
   echo '<option value="' . $faction->id . '"';
@@ -45,7 +49,7 @@ foreach ($this->factions as $faction) {
       <tr>
         <td></td>
         <td>
-          <input type="submit" value="Skapa grupp" />
+          <input type="submit" value="<?php echo JText::_('COM_LAJVIT_GROUP_SAVE'); ?>" />
           <input type="hidden" name="option" value="com_lajvit" />
           <input type="hidden" name="task" value="create" />
           <input type="hidden" name="controller" value="group" />
